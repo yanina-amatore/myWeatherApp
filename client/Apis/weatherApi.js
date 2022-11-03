54b3a77caa4c87ce4e6558eb6dbbb7755d2c2022
Yanina Amatore <yaninaamatore@gmail.com>
2022-11-04 11:22:29 +1300

    working on moving API weather call to API file

--- client/Apis/weatherApi.js
+++ client/Apis/weatherApi.js
@@ -1,4 +1,28 @@
 
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export function getWeatherData(searchData){
+  
+  const lat = searchData.value.lat 
+  const lon = searchData.value.lon
 
+  const weatherFetch = fetch(
+    `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+  );
+  const forecastFetch = fetch(
+    `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+  );
+  
+  Promise.all([weatherFetch, forecastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forcastResponse = await response[1].json();
+      
+      const resAPI ={ weatherResponse: weatherResponse, forcastResponse: forcastResponse}
+      console.log('resapi', resAPI)
+      return resAPI
+    })
+    .catch(console.log);
+
+   
+}
