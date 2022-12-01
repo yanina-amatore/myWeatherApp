2562f80a0cc36c69cb83c82c9d8fc414f2c009f0
Yanina Amatore <yaninaamatore@gmail.com>
2022-12-02 09:37:52 +1300

    add key to Forecast return

--- client/components/Forecast.jsx
+++ client/components/Forecast.jsx
@@ -27,48 +27,46 @@ function Forecast({ data }) {
             const time = [str[4]]
 
             return (
-              <>
-                <div
-                  key={idx}
-                  className="cards p-4 bg-white"
-                  style={{ width: '1,6rem' }}
-                >
-                  <div key={idx} className="card-body">
-                    <div>
-                      <p className="fs-3 text-center ">
-                      <b>{newDate}</b>
-                      </p>
-                      <p className="fs-3 text-center">{time}</p>
-                    </div>
-                    <hr/>
-                  </div>
-                  <figure className='d-flex justify-content-center'>
-                    <img
-                      className="text-center"
-                      alt="weather"
-                      src={`../img/${i.weather[0].icon}.png`}
-                    />
-                  </figure>
+              <div
+                key={idx}
+                className="cards p-4 bg-white"
+                style={{ width: '1,6rem' }}
+              >
+                <div className="card-body">
                   <div>
-                    <div>
-                      <i className="fa-solid fa-temperature-arrow-up text-danger"></i>
+                    <p className="fs-3 text-center ">
+                      <b>{newDate}</b>
+                    </p>
+                    <p className="fs-3 text-center">{time}</p>
+                  </div>
+                  <hr />
+                </div>
+                <figure className="d-flex justify-content-center">
+                  <img
+                    className="text-center"
+                    alt="weather"
+                    src={`../img/${i.weather[0].icon}.png`}
+                  />
+                </figure>
+                <div>
+                  <div>
+                    <i className="fa-solid fa-temperature-arrow-up text-danger"></i>
 
-                      <span className="p-4 fs-5 "> {i.main.temp_max} °C</span>
-                    </div>
-                    <div>
-                      <i className="fa-solid fa-temperature-arrow-down text-primary"></i>
-                      <span className=" p-4 fs-5"> {i.main.temp_min} °C</span>
-                    </div>
-                    <div className='py-3'>
-                      <span className="text-muted  ">Humidity</span>
-                      <span className=" text-muted p-4">
-                        {' '}
-                        {i.main.humidity} %{' '}
-                      </span>
-                    </div>
+                    <span className="p-4 fs-5 "> {i.main.temp_max} °C</span>
+                  </div>
+                  <div>
+                    <i className="fa-solid fa-temperature-arrow-down text-primary"></i>
+                    <span className=" p-4 fs-5"> {i.main.temp_min} °C</span>
+                  </div>
+                  <div className="py-3">
+                    <span className="text-muted  ">Humidity</span>
+                    <span className=" text-muted p-4">
+                      {' '}
+                      {i.main.humidity} %{' '}
+                    </span>
                   </div>
                 </div>
-              </>
+              </div>
             )
           })}
         </div>
--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Weather({ data }) {
-  console.log(data, 'data')
+  console.log( 'data', data)
 
   return (
     <>
