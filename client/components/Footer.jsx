a93331a29e5e2c0f9fad78d6442dde258425a9af
Yanina Amatore <yaninaamatore@gmail.com>
2022-12-02 10:00:47 +1300

    clean App component, created Main and Footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function Footer() {
+
+  return (
+
+<section className='section-footer '>
+      <div className=" bg-dark bg-gradient p-2">
+      <span className='d-flex justify-content-center'>
+        <a
+          className="text-decoration-none text-dark text-capitalize text-white"
+          href="https://github.com/yanina-amatore"
+        >
+          <i className="fa-brands fa-github fa-lg mx-2"></i>
+          created by yanina-amatore
+        </a>
+      </span>
+
+      </div>
+    </section>
+  )
+}
+
+export default Footer
+
